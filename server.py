6faa4eab20f5cfe88b23bfdfa42b73aba67c3ee6
AP1028 <tian_yi_xia@qq.com>
2025-07-28 14:49:15 -0700

    fix: minor fix

--- server.py
+++ server.py
@@ -357,8 +357,10 @@ class BackendServer:
         # check if everything is correct with modality
         for offset in range(0,self.cam_cnt):
             verify_cam_name = self.data_list[offset][self.tag_entry_cam]
+            log_info(f'verify_cam_name is {verify_cam_name} at {offset}')
             for i in range(offset, self.data_cnt, self.cam_cnt):
                 if self.data_list[i][self.tag_entry_cam] != verify_cam_name:
+                    log_info(f'mismatch at {self.tag_entry_cam} with name of {self.data_list[i][self.tag_entry_cam]}')
                     self.cam_cnt = -1
                     break
                 
